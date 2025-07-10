e4a44308037ce29d2a110445e29965535fdaee26
Preksha <prekshadivraniya09@gmail.com>
2025-07-10 09:15:04 +0530

    Effectively handled the fizzbuzz challange with all edge cases and with full coverage

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class FizzBuzz {
+public class FizzBuzz {
+    //divisible by 3 return Fizz
+    //divisible by 5 return Buzz
+    //divisible by 3 and 5 return FizzBuzz
+    //otherwise return number that is inputed
+    public Object returnFizzBuzz(int input){
+        if(input <= 0){
+            return input;
+        }
+
+        if(input % 3 == 0){
+            if(input % 5 == 0) return "FizzBuzz";
+            else return "Fizz";
+        }else if(input % 5 == 0) return "Buzz";
+        return input;
+    }
 }
--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,4 +1,42 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FizzBuzzTest {
-  
+
+    @Test
+    void given0Expect0(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals(0,checkFizzBuzz.returnFizzBuzz(0));
+    }
+
+    @Test
+    void given1Expect1(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals(1,checkFizzBuzz.returnFizzBuzz(1));
+    }
+
+    @Test
+    void given3ExpectFizz(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals("Fizz",checkFizzBuzz.returnFizzBuzz(3));
+    }
+
+    @Test
+    void given30ExpectFizz(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals("FizzBuzz",checkFizzBuzz.returnFizzBuzz(30));
+    }
+
+    @Test
+    void given5ExpectBuzz(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals("Buzz",checkFizzBuzz.returnFizzBuzz(5));
+    }
+
+    @Test
+    void given6ExpectFizz(){
+        var checkFizzBuzz = new FizzBuzz();
+        assertEquals("Fizz",checkFizzBuzz.returnFizzBuzz(6));
+    }
 }
