132f0c5ddb42d4f401c93583d2b256193431e1ce
Preksha <prekshadivraniya09@gmail.com>
2025-07-10 18:02:36 +0530

    Practice the importance of BeforeEach and AfterEach nnotations in testing

--- BeforeEachAnnotation/src/main/java/AreaCalculation.java
+++ BeforeEachAnnotation/src/main/java/AreaCalculation.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class AreaCalculation {
+public class AreaCalculation {
+    public double computeSquareArea(double length){
+        if(length < 0)
+            throw new IllegalArgumentException("Length cannot be negative");
+        return length * length;
+    }
+    public double computeCircleArea(double radius){
+        if(radius < 0)
+            throw new IllegalArgumentException("Radius cannot be negative");
+        return 3.14 * radius * radius ;
+    }
 }
--- BeforeEachAnnotation/src/test/java/AreaCalculationTest.java
+++ BeforeEachAnnotation/src/test/java/AreaCalculationTest.java
@@ -1,4 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AreaCalculationTest {
-  
+
+    AreaCalculation area;
+
+    @BeforeEach
+    void init(){
+        area = new AreaCalculation();
+    }
+
+    @Test
+    void testComputeSquareArea(){
+        assertEquals(49,area.computeSquareArea(7));
+    }
+
+    @Test
+    void testComputeCircleArea(){
+        assertEquals(78.5, area.computeCircleArea(5));
+    }
+
+    @Test
+    void throwExceptionInComputeSquareArea(){
+        assertThrows(IllegalArgumentException.class, () -> area.computeSquareArea(-7));
+    }
+
+    @Test
+    void throwExceptionInComputeCircleArea(){
+        assertThrows(IllegalArgumentException.class, ()->area.computeCircleArea(-2.5));
+    }
+
+    @AfterEach
+    void destroy(){
+        System.out.println("After test clean up");
+    }
 }
