fdd0fe493ff855b449a20e3241fab916fc5cb84a
Preksha <prekshadivraniya09@gmail.com>
2025-07-11 13:21:53 +0530

    Completed MarsRover TDD Kata

--- MarsRover/src/main/java/MarsRover.java
+++ MarsRover/src/main/java/MarsRover.java
@@ -1,2 +1,53 @@
 public class MarsRover {
+
+    private int x;
+    private int y;
+    private String[] direction = {"N", "E", "S", "W"} ;
+    private int index = 0;
+
+    MarsRover(int x, int y, String direction){
+        this.x = x;
+        this.y = y;
+        switch (direction){
+            case "N": index = 0;break;
+            case "S": index = 2;break;
+            case "E": index = 1;break;
+            case "W": index = 3;break;
+        }
+    }
+
+    public String getPosition(){
+        return x + " " + y + " " + direction[index];
+    }
+
+    public void execute(String moves){
+        for(char move : moves.toCharArray()){
+            if(move != 'M' && move != 'R' && move != 'L'){
+                System.out.println("Ignored unknown command: " + move);
+                continue;
+            }
+            switch (move){
+                case 'M' : move(); break;
+                case 'L' : turnLeft(); break;
+                case 'R' : turnRight(); break;
+            }
+        }
+    }
+
+    public void move(){
+        switch (index){
+            case 0 : y++; break;
+            case 2 : y--; break;
+            case 1 : x++; break;
+            case 3 : x--; break;
+        }
+    }
+
+    public void turnLeft(){
+        index = (index + 3) % 4;
+    }
+
+    public void turnRight(){
+        index = (index + 1) % 4;
+    }
 }
--- MarsRover/src/test/java/MarsRoverTest.java
+++ MarsRover/src/test/java/MarsRoverTest.java
@@ -1,4 +1,59 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MarsRoverTest {
-  
+    MarsRover rover;
+    @BeforeEach
+    void init(){
+        rover = new MarsRover(0,0,"N");
+    }
+    @Test
+    void shouldInitializeRoverAtGivenPositionAndDirection(){
+        assertEquals("0 0 N",rover.getPosition());
+    }
+
+    @Test
+    void moveRoverInTheSameDirection(){
+        rover.execute("M");
+        assertEquals("0 1 N",rover.getPosition());
+    }
+
+    @Test
+    void turnDirectionToRight(){
+        rover.execute("R");
+        assertEquals("0 0 E",rover.getPosition());
+    }
+
+    @Test
+    void givenSomeMovesToGetDesiredPostionAndDirection(){
+        rover.execute("MRMLMLL");
+        assertEquals("1 2 S",rover.getPosition());
+    }
+
+    @Test
+    void checkFullCircleUsingLeft(){
+        rover.execute("LLLL");
+        assertEquals("0 0 N", rover.getPosition());
+    }
+
+    @Test
+    void shouldReturnToOriginalPositionAfterSquarePath() {
+        rover.execute("MRMRMRMR"); // Move & turn right in a square
+        assertEquals("0 0 N", rover.getPosition());
+    }
+
+    @Test
+    void shouldCoverAllLinesOfCode(){
+        rover.execute("MXL");
+        assertEquals("0 1 W", rover.getPosition());
+    }
+
+    @AfterAll
+    static void destroy(){
+        System.out.println("All tests are passed");
+    }
 }
